aa114b5e7eed9999d570116c42b6b90bce599206
imdeepmind <infinityatme@gmail.com>
2019-02-12 21:55:32 +0530

    added rest api

---
+++ rest-api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import gensim
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+def index():
+    return jsonify({
+        "success" : True
+    })
+
+
+@app.route("/similar/", methods=['GET'])
+def similar():
+    try:
+        word = request.args.get('word')
+        model = gensim.models.KeyedVectors.load_word2vec_format('model.bin', binary=True)
+        data = model.wv.most_similar(word)
+        similar = json.dumps(data, separators=(',',':'))
+        return jsonify({
+            "success" : True,
+            "data": data
+        })
+    except Exception as ex:
+        print(ex)
+        return jsonify({
+            "success": False,
+            "error" : "Something went wrong"
+        })
